dcc35496c549016167661d5e14dbc464d34acd78
Javs C <javcolin1@gmail.com>
2020-09-11 16:23:45 -0700

    adding functionality of datePicker

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Search.css"
+// main css file
+import "react-date-range/dist/styles.css";
+// theme css file 
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
 
 
 // datePicker Component
 
 function Search() {
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	const selectionRange = {
+			startDate: startDate,
+			endDate: endDate,
+			key: "selection"
+		};
+
+function handleSelect(ranges) {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	}
+
 	return (
 		<div className ="search">
-			Here is the search feature
+			 <DateRangePicker ranges={
+			 	[selectionRange]} onChange= {handleSelect} />
+			 }
+
 		</div>
 	)
 }
