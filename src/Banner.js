97449296cc27ef57d94317f5a0e101438564fa3b
Javs C <javcolin1@gmail.com>
2020-09-11 00:02:53 -0700

    adding a new change

--- src/Banner.js
+++ src/Banner.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from "@material-ui/core";
+import Search from "./Search"
+
 
 function Banner() {
+	const [showSearch, setShowSearch] = useState(false);
+
 	return (
 		<div className="banner">
-			<div className="banner_info">
-			<h1>Get out and stretch your imagination</h1>
-			<h5>
-				Go out an plan an adventure to uncover the 
-				hidden wonders of the work 
-			</h5>
-			 <Button variant="outlined">
-				Explore Nearby
-			</Button>
+			<div className="banner_search">
+				{showSearch && <Search /> }
+				<Button onClick={()=> 
+					setShowSearch(!showSearch)}
+				className="banner_searchButton" variant="outlined">
+
+					Search Dates
+					
+				</Button>
 			</div>
-		</div>
+					
+					<div className="banner_info">
+					
+						<h1>
+						Get out and stretch your imagination
+						</h1>
+						<h5>
+						
+						Go out an plan an adventure to uncover the 
+						hidden wonders of the work 
+						</h5>
+					 <Button variant="outlined">
+						Explore Nearby
+					</Button>
+					</div>
+			</div>
 	)
 }
 
---
+++ src/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Search.css"
+
+
+// datePicker Component
+
+function Search() {
+	return (
+		<div className ="search">
+			Here is the search feature
+		</div>
+	)
+}
+
+export default Search
